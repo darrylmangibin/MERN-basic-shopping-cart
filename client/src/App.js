e8a5d382d8e91f70ac891005fd4be3da9b5ad488
Darryl <darrylmangibin@gmail.com>
2019-06-15 23:09:10 +0800

    ShoppingList Component and Transitions

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
